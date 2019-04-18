137eaffa728574aee93c22c1955de8ea1299e6d4
xiaoxuntc <18399104@qq.com>
2019-04-18 23:44:58 +0800

    seventeenth 收录电影分页显示

--- src/main/java/com/java1234/controller/FilmController.java
+++ src/main/java/com/java1234/controller/FilmController.java
@@ -5,8 +5,10 @@ import java.util.List;
 import javax.annotation.Resource;
 import javax.validation.Valid;
 
+import com.java1234.util.PageUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -52,6 +54,28 @@ public class FilmController {
             mav.addObject("total", filmList.size());
             mav.setViewName("index");
         }
+        return mav;
+    }
+
+    /***
+     * @Description : 分页查询电影信息
+     * @Method_Name : list
+     * @Param :  @param page
+     * @return : org.springframework.web.servlet.ModelAndView
+     * @Creation Date : 2019/4/17
+     * @Author : Sean
+     */
+    @RequestMapping("/list{id}")
+    public ModelAndView list(@PathVariable(value = "id", required = false) Integer page) throws Exception {
+        ModelAndView mav = new ModelAndView();
+        List<Film> filmList = filmService.list(null, page - 1, 32);
+        Long total = filmService.getCount(null);
+        mav.addObject("filmList", filmList);
+        mav.addObject("pageCode", PageUtil.genPagination("/film/list", total, page, 20));
+        mav.addObject("title", "电影列表");
+        mav.addObject("mainPage", "film/list");
+        mav.addObject("mainPageKey", "#f");
+        mav.setViewName("index");
         return mav;
     }
 }
